e776e667c443170f237191c4445fafaf3437d858
sammed nj <sammednj13@gmail.com>
2023-06-17 16:53:27 +0530

    setting react dom

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import './App.css'
+import Navbar from './assets/components/Navbar'
+import Footer from './assets/components/Footer'
+import Home from './assets/pages/Home'
+import Add from './assets/pages/Add'
+import Gig from './assets/pages/Gig'
+import Gigs from './assets/pages/Gigs'
+import Login from './assets/pages/Login'
+import Message from './assets/pages/Message'
+import Messages from './assets/pages/Messages'
+import MyGigs from './assets/pages/MyGigs'
+import Orders from './assets/pages/Orders'
+import Register from './assets/pages/Register'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+function App() {
+
+
+  const Layout = () => {
+    return (
+      <div className='app'>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    )
+  };
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/add",
+          element: <Add />
+        },
+        {
+          path: "/gig",
+          element: <Gig />
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />
+        },
+        {
+          path: "/login",
+          element: <Login />
+        },
+        {
+          path: "/message",
+          element: <Message />
+        },
+        {
+          path: "/messages",
+          element: <Messages />
+        },
+        {
+          path: "/myGigs",
+          element: <MyGigs />
+        },
+        {
+          path: "/orders",
+          element: <Orders />
+        },
+        {
+          path: "/register",
+          element: <Register />
+        },
+      ]
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react'
+// import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+    const [active, setActive] = useState(false); // State variable to track if navbar is active
+    const [open, setOpen] = useState(false); // State variable to track if user options dropdown is open
+
+    const isActive = () => {
+        window.scrollY > 0 ? setActive(true) : setActive(false); // Check if user has scrolled down and update active state accordingly
+    }
+
+    useEffect(() => {
+        // Add event listener to track scroll position and update active state
+        window.addEventListener("scroll", isActive);
+        return () => {
+            // Clean up event listener when component unmounts
+            window.removeEventListener("scroll", isActive);
+        }
+    }, []);
+
+    const currentUser = {
+        id: 1,
+        username: "Jone Doe",
+        isSeller: true
+    }
+
+    return (
+        <div className={`navbar ${active ? "bg-white text-black" : "bg-green-900 text-white"} sticky top-0 flex items-center flex-col transition-all duration-200 ease-in`} >
+            <div className='w-4/5 flex justify-between py-4 '>
+                <div className='text-4xl'>
+                    {/* <Link to='/'> */}
+                    <span className='font-bold'>fiverr</span>
+                    <span className='text-green-600'>.</span>
+                    {/* </Link> */}
+                </div>
+                <div className='flex gap-4 items-center font-medium'>
+                    <ul className='flex gap-4 items-center relative'>
+                        <li className='cursor-pointer'>Fiverr Business</li>
+                        <li className='cursor-pointer'>Explore</li>
+                        <li className='cursor-pointer'>English</li>
+                        <li className='cursor-pointer'>Sign in</li>
+                        {!currentUser?.isSeller && <li> Become a Seller</li>}
+                        {currentUser && (
+                            <li className='user flex items-center justify-center cursor-pointer' onClick={() => setOpen(!open)}>
+                                <img src="/src/assets/imgs/account.png" alt="user_icon" className='w-12 h-12' />
+                                <span>{currentUser?.username}</span>
+                                {open && <div className='options p-3 absolute top-12 right-2 rounded bg-gray-50 w-[200px] overflow-hidden'>
+                                    <ul className='text-gray-500 text-sm font-normal flex flex-col gap-3'>
+                                        {
+                                            currentUser?.isSeller && (
+                                                <>
+                                                    <li className='cursor-pointer'>Gigs</li>
+                                                    <li className='cursor-pointer'>Add Gigs</li>
+                                                </>
+                                            )
+                                        }
+                                        <li className='cursor-pointer'>Orders</li>
+                                        <li className='cursor-pointer'>Messages</li>
+                                        <li className='cursor-pointer'>Logout</li>
+                                    </ul>
+                                </div>}
+                            </li>
+                        )}
+                    </ul>
+                    {!currentUser && <button className={`${active ? "border-green-600 text-green-600 hover:text-white" : "border-white"} border-2  rounded py-1 px-3 bg-transparent hover:bg-green-600 hover:border-green-600 cursor-pointer`}>Join</button>}
+                </div>
+            </div>
+            {
+                active && (
+                    <>
+                        <hr className='w-full h-0 border-1 border-gray-200' />
+                        <div className='w-4/5 text-sm'>
+                            <ul className=' flex justify-between items-center py-4 '>
+                                <li>Graphic & Design</li>
+                                <li>Video & Animation</li>
+                                <li>Writing & Translation</li>
+                                <li>AI Services</li>
+                                <li>Digital Marketing</li>
+                                <li>Music & Audio</li>
+                                <li>Programming & Tech</li>
+                                <li>Business</li>
+                                <li>Lifestyle</li>
+                            </ul>
+                        </div>
+                    </>
+                )
+            }
+        </div>
+    )
+}
+
+export default Navbar
